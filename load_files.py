587531fa7f69ed9880c958c39877bee1100b0cf5
MihailStamenov98 <mpstamenov@gmail.com>
2024-06-28 17:25:43 +0300

    fix main

--- load_files.py
+++ load_files.py
@@ -4,10 +4,7 @@ import json
 
 def list_json_files(directory):
     try:
-        # List all files in the given directory
         files = os.listdir(directory)
-
-        # Filter out and return only the JSON files
         json_files = [file for file in files if file.endswith(".json")]
         return json_files
     except FileNotFoundError:
@@ -20,20 +17,21 @@ def load_json(file_path):
         return json.load(file)
 
 
-def load_all_files():
+def load_all_files(directory="data/data_pool/"):
     while True:
         file_name = input(
             "Please enter the name of the file containing the texts (press Enter to use 'app1.2.json'): "
         )
         if not file_name:
-            file_name = "app1.2.json"
+            file_name = "data/query_data/app1.2.json"
         if os.path.isfile(file_name):
             break  # Exit the loop if the file is successfully read
         else:
             print(
                 f"File '{file_name}' not found. Please make sure the file exists and try again."
             )
-    json_files = ["app1.json", "app2.json"]
-    json_objects = [load_json(file) for file in json_files]
+    json_files = list_json_files(directory=directory)
+    prefixed_files = [f"{directory}{file}" for file in json_files]
+    json_objects = [load_json(file) for file in prefixed_files]
     query_json = load_json(file_name)
-    return json_objects, query_json
+    return [json_objects[0]], query_json
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import time
-from functools import partial
-from predictors.pretrained_models.encoders import *
-from predictors.TfidfVectorizer_encoder import *
-from predictors.graph_similarity import GraphSimilarity
+from predictors import GraphSimilarity, TF_IDF, Encoders
 from load_files import load_all_files
 
 
@@ -26,47 +23,26 @@ def main():
         choice = input("Please enter the number of the solution you want to use: ")
 
     json_objects, query_json = load_all_files()
-
+    model = None
     if choice == "1":
         print("You chose encoding with BERT.")
-        most_similar = get_most_similar(
-            json_objects=json_objects,
-            query_json=query_json,
-            feature_extraction_func=partial(
-                transformer_feature_extractor, model="bert"
-            ),
-            embedings_path="bert_encodings",
-        )
+        model = Encoders("bert")
     elif choice == "2":
         print("You chose encoding with Electra.")
-        most_similar = get_most_similar(
-            json_objects=json_objects,
-            query_json=query_json,
-            feature_extraction_func=partial(
-                transformer_feature_extractor, model="electra"
-            ),
-            embedings_path="electra_encodings",
-        )
+        model = Encoders("electra")
     elif choice == "3":
         print("You chose encoding with TF-IDF.")
-        most_similar = tf_idf_encoding_compare(
-            json_objects=json_objects, query_json=query_json
-        )
+        model = TF_IDF(json_objects)
     elif choice == "4":
         print("You chose Graph similarity.")
-        gs = GraphSimilarity(json_objects=json_objects)
-        most_similar = gs.predict(query_json=query_json)
+        model = GraphSimilarity(json_objects=json_objects)
     else:
         print("Invalid choice. Please enter 1, 2, or 3.")
-    end_time = time.time()
-
-    print(most_similar.get("ai_dict", {}).get("name", "Unknown App"))
-    print("Elapsed time: ", end_time - start_time)
+        return
+    most_similar = model.predict(query_json=query_json)
+    print(most_similar)
+    print("Elapsed time: ", time.time() - start_time)
 
 
 if __name__ == "__main__":
-    # json_files = ["app1.json", "app2.json"]
-    # json_objects = [load_json(file) for file in json_files]
-    # query_json = load_json('app1.2.json')
-    # most_similar = graph_sim(json_objects=json_objects, query_json=query_json)
     main()
